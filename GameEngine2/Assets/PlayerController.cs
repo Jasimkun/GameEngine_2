5a54cc06e0ff3ba6805182a9bdd0097342f50ee3
jasimkun <lse061123@gmail.com>
2025-09-16 16:19:25 +0900

    [Feat] 3강 과제

--- GameEngine2/Assets/PlayerController.cs
+++ GameEngine2/Assets/PlayerController.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed;
 
-    public float speed = 5f;
+    private float walkSpeed = 5f;
 
-    public float jumpPower = 5f;
+    private float runSpeed = 12f;
+
+    private float stopSpeed = 0f;
+
+    private float jumpPower = 5f;
+
+    private float stopJumpPower = 0f;
+
+    public CinemachineSwitcher cinemachineSwitcher;
 
     public float gravity = -9.81f;
+
+    public CinemachineVirtualCamera virtualCam;
+
+    public float rotationSpeed = 10f;
+
+    private CinemachinePOV pov;
 
     private CharacterController controller;
 
@@ -21,22 +38,66 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        pov = virtualCam.GetCinemachineComponent<CinemachinePOV>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = controller.isGrounded;
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        if (cinemachineSwitcher.usingFreeLook == true)
+        {
+            speed = stopSpeed;
+            jumpPower = stopJumpPower;
+        }
+        else
+        {
+            jumpPower = 5f;
 
-        Vector3 move = new Vector3(x, 0, z);
-        controller.Move(move * speed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = runSpeed;
+                virtualCam.m_Lens.FieldOfView = 80f;
+            }
+            else
+            {
+                speed = walkSpeed;
+                virtualCam.m_Lens.FieldOfView = 60f;
+            }
+        }
 
+        //점프
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             velocity.y = jumpPower;
         }
+
+
+        //땅에 닿아 있는지 확인
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;  //지면에 붙이기
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //카메라 기준 방향 계산
+        Vector3 camForward = virtualCam.transform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        Vector3 camRight = virtualCam.transform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+
+        Vector3 move = (camForward * z + camRight * x).normalized;  //이동 방향 = 카메라 forward/right 기반
+        controller.Move(move * speed * Time.deltaTime);
+
+        float cameraYaw = pov.m_HorizontalAxis.Value;   //마우스 좌우 회전값
+        Quaternion targetRot = Quaternion.Euler(0f, cameraYaw, 0f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotationSpeed * Time.deltaTime);
+
+
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
